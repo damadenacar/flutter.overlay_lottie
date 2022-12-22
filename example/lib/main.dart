2b95ef4bd99217593f9b0c005476d4ab666f824b
damadenacar <damadenacar@gmail.com>
2022-12-22 14:13:37 +0100

    reformat code

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -24,73 +24,40 @@ class LottieOverlayHome extends StatefulWidget {
   State<LottieOverlayHome> createState() => _LottieOverlayHomeState();
 }
 
-class _LottieOverlayHomeState extends State<LottieOverlayHome> with TickerProviderStateMixin, OverlayLottie {
+class _LottieOverlayHomeState extends State<LottieOverlayHome>
+    with TickerProviderStateMixin, OverlayLottie {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: null,
-      body: buildWithLottieOverlay(context, 
-          animationUrl: "https://assets6.lottiefiles.com/packages/lf20_fj8rlma5.json",
+      body: buildWithLottieOverlay(
+          animationUrl:
+              "https://assets6.lottiefiles.com/packages/lf20_fj8rlma5.json",
           child: Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 ElevatedButton(
-                        onPressed: () => showLottieOverlay(repetitions: 1),
-                        child: const Text("show Lottie")
-                      ),
+                    onPressed: () => showLottieOverlay(repetitions: 1),
+                    child: const Text("show Lottie")),
                 ElevatedButton(
-                        onPressed: () {
-                          showLottieOverlayWhile(
-                            () async {
-                              await Future.delayed(const Duration(seconds: 3));
-                            },  
-                            animationUrl: "https://assets6.lottiefiles.com/packages/lf20_z3pnisgt.json", 
-                          ).then((value) {
-                            showLottieOverlay(animationUrl: "https://assets8.lottiefiles.com/packages/lf20_slGFhN.json");
-                          });
+                    onPressed: () {
+                      showLottieOverlayWhile(
+                        () async {
+                          await Future.delayed(const Duration(seconds: 3));
                         },
-                        child: const Text("simulate login")
-                      ),
+                        animationUrl:
+                            "https://assets6.lottiefiles.com/packages/lf20_z3pnisgt.json",
+                      ).then((value) {
+                        showLottieOverlay(
+                            animationUrl:
+                                "https://assets8.lottiefiles.com/packages/lf20_slGFhN.json");
+                      });
+                    },
+                    child: const Text("simulate login")),
               ],
             ),
-          )
-          /*
-          child: SizedBox.expand(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                  Text("Overlay Lottie\nDemo", style: Theme.of(context).textTheme.headlineLarge, textAlign: TextAlign.center),
-                  const SizedBox(height: 16,),
-                  ElevatedButton(onPressed: () {
-                      showLottieOverlay(repetitions: 1);
-                    },
-                    child: const Text("show Lottie 1 repetition")
-                  ),
-                  ElevatedButton(onPressed: () {
-                      showLottieOverlay(repetitions: 3, onHide: () {
-                        print("run 3 times");
-                      },);
-                    },
-                    child: const Text("show Lottie 3 repetitions")
-                  ),
-                  ElevatedButton(onPressed: () {
-                      print(showLottieOverlayDuring(const Duration(seconds: 2)));
-                      print("continuo");
-                    },
-                    child: const Text("show Lottie for 2 seconds")
-                  ),
-                  ElevatedButton(onPressed: () {
-                      showLottieOverlayWhile(() async {
-                        await Future.delayed(const Duration(seconds: 5));
-                      }, ).then((value) { if (value) print("closed after 5 seconds"); });
-                    },
-                    child: const Text("show Lottie during the execution of a function")
-                  ),
-                ]
-            )
-          )*/
-        ),
-      );
+          )),
+    );
   }
 }
