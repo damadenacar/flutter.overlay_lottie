f84cdc541bb1005745b8e0ebd5eab055113dc725
damadenacar <damadenacar@gmail.com>
2022-12-22 14:12:52 +0100

    remove context and reformat

--- lib/overlay_lottie.dart
+++ lib/overlay_lottie.dart
@@ -5,9 +5,9 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
-/// Mixin that helps to overlay a Lottie animation over a content upon request (i.e. call of [showLottieOverlay], [showLottieOverlayDuring] 
+/// Mixin that helps to overlay a Lottie animation over a content upon request (i.e. call of [showLottieOverlay], [showLottieOverlayDuring]
 ///   or [showLottieOverlayWhile]). It enables a control on the repetitions of the animation.
-/// 
+///
 mixin OverlayLottie<T extends StatefulWidget> on TickerProviderStateMixin<T> {
   // The animation controller
   late final AnimationController _controller;
@@ -30,6 +30,7 @@ mixin OverlayLottie<T extends StatefulWidget> on TickerProviderStateMixin<T> {
   // An URL that enables to override the default animation
   String? _animationUrlOverride;
 
+  // The object that enables the future to be resolved in different calls (e.g. when the object is hidden programmatically)
   late Completer<bool> _completer;
 
   @override
@@ -63,60 +64,75 @@ mixin OverlayLottie<T extends StatefulWidget> on TickerProviderStateMixin<T> {
   }
 
   /// Shortcut to show the animation during a [period].
-  /// 
+  ///
   /// It returns true if the callback is started; false otherwise (i.e. if the animation is already shown)
-  Future<bool> showLottieOverlayDuring(Duration period, { String? animationUrl, VoidCallback? onHide }) {
-    return showLottieOverlayWhile(
-      () async => await Future.delayed(period), 
-      onHide: onHide, animationUrl: animationUrl);
+  Future<bool> showLottieOverlayDuring(Duration period,
+      {String? animationUrl, VoidCallback? onHide}) {
+    return showLottieOverlayWhile(() async => await Future.delayed(period),
+        onHide: onHide, animationUrl: animationUrl);
   }
 
   /// Shortcut to show the animation while a [callback] is executed. If provided, after [timeout] period, the execution
   ///   of [callback] will be aborted and (if provided), function [onTimeout] will be called.
-  /// 
+  ///
   /// It returns true if the callback is started; false otherwise (i.e. if the animation is already shown)
-  Future<bool> showLottieOverlayWhile(Function callback, {Duration? timeout, Function? onTimeout, String? animationUrl, VoidCallback? onHide}) async {
+  Future<bool> showLottieOverlayWhile(Function callback,
+      {Duration? timeout,
+      Function? onTimeout,
+      String? animationUrl,
+      VoidCallback? onHide}) async {
     if (_visible) {
       return false;
     }
 
-    _showLottieOverlay(repetitions: double.maxFinite.toInt(), animationUrl: animationUrl, onHide: onHide);
+    _showLottieOverlay(
+        repetitions: double.maxFinite.toInt(),
+        animationUrl: animationUrl,
+        onHide: onHide);
     if (timeout == null) {
       await callback();
     } else {
       await callback().timeout(timeout, onTimeout: () async {
-          if (onTimeout != null) {
-            onTimeout();
-          }
-        });
+        if (onTimeout != null) {
+          onTimeout();
+        }
+      });
     }
 
     hideLottieOverlay();
     return true;
   }
 
-  Future<bool> showLottieOverlay({bool? force, int? repetitions, String? animationUrl, VoidCallback? onHide}) async {
+  Future<bool> showLottieOverlay(
+      {bool? force,
+      int? repetitions,
+      String? animationUrl,
+      VoidCallback? onHide}) async {
     if (!_showLottieOverlay(
-      animationUrl: animationUrl,
-      repetitions: repetitions,
-      onHide: onHide,
-      force: force??false)) {
-        return false;
-      }
+        animationUrl: animationUrl,
+        repetitions: repetitions,
+        onHide: onHide,
+        force: force ?? false)) {
+      return false;
+    }
     return _completer.future;
   }
 
   /// Shows the animation for [repetitions] time. If not provided, the amount of repetitions is the one provided during the
   ///   creation of the widget. If the animation is already shown, the state is not changed unless [forced] is set to true.
-  bool _showLottieOverlay({String? animationUrl, bool force = false, int? repetitions, VoidCallback? onHide}) {
-    if (_visible && ! force) {
+  bool _showLottieOverlay(
+      {String? animationUrl,
+      bool force = false,
+      int? repetitions,
+      VoidCallback? onHide}) {
+    if (_visible && !force) {
       return false;
     }
     _completer = Completer();
     setState(() {
       _visible = true;
-      _repetitions = repetitions??_defaultNumberOfRepetitions;
-      _onHide = onHide??_defaultOnHide;
+      _repetitions = repetitions ?? _defaultNumberOfRepetitions;
+      _onHide = onHide ?? _defaultOnHide;
       _animationUrlOverride = animationUrl;
     });
     return true;
@@ -124,12 +140,12 @@ mixin OverlayLottie<T extends StatefulWidget> on TickerProviderStateMixin<T> {
 
   /// Stops the animation. If the animation is not being shown, the state is not changed unless [forced] is set to true.
   void hideLottieOverlay([bool force = false]) {
-    if (!_visible && ! force) {
+    if (!_visible && !force) {
       _completer.complete(false);
       return;
     }
 
-    VoidCallback? onHide = _onHide??_defaultOnHide;
+    VoidCallback? onHide = _onHide ?? _defaultOnHide;
 
     setState(() {
       _visible = false;
@@ -145,44 +161,44 @@ mixin OverlayLottie<T extends StatefulWidget> on TickerProviderStateMixin<T> {
   }
 
   /// Builds the structure of the widget with an animation. The content shown is [child], but if the animation is being shown,
-  ///   and [animationUrl] is provided, then a lottie animation is overlayed and the child is deactivated. 
-  /// 
-  /// If provided, the [durationPerRepetition] is set for each loop of the animation (i.e. to force that an animation loop 
+  ///   and [animationUrl] is provided, then a lottie animation is overlayed and the child is deactivated.
+  ///
+  /// If provided, the [durationPerRepetition] is set for each loop of the animation (i.e. to force that an animation loop
   ///   lasts a specific time); if not provided, the duration of each loop is the duration of the lottie object. When simply
   ///   shown an animation, it will be repeated for [defaultNumberOfRepetitions], unless a specific number of repetitions is
   ///   set when showing the animation.
-  /// 
+  ///
   /// If the animation is being show, the content is blurred (if [blurContent] is set to true), and such content is also converted
   ///   to semi-transparent, according to the value of [opacity] (default: 0.8)
-  /// 
-  Widget buildWithLottieOverlay(BuildContext context, { 
-      required Widget child, 
-      String? animationUrl, 
-      Duration? durationPerRepetition, 
-      int defaultNumberOfRepetitions = 1, 
-      bool blurContent = false, 
+  ///
+  Widget buildWithLottieOverlay(
+      {required Widget child,
+      String? animationUrl,
+      Duration? durationPerRepetition,
+      int defaultNumberOfRepetitions = 1,
+      bool blurContent = false,
       double opacity = 0.8,
-      VoidCallback? onHide
-    }) {
-
+      VoidCallback? onHide}) {
     // Capture the parameters
     Widget content = child;
     _defaultNumberOfRepetitions = defaultNumberOfRepetitions;
     _defaultOnHide = onHide;
 
     // Override the default animation, if set
-    animationUrl = _animationUrlOverride??animationUrl;
+    animationUrl = _animationUrlOverride ?? animationUrl;
     _animationUrlOverride = null;
 
     // If the animation is visible and a URL has been provided, let's create the animation and the overlay
     if (_visible && (animationUrl != null)) {
-
       // Add opacity and blur (if requested)
       if (opacity != 1.0) {
         content = Opacity(opacity: 0.5, child: content);
       }
       if (blurContent) {
-        content = ImageFiltered(imageFilter: ImageFilter.blur(sigmaX: 1, sigmaY: 1), child: content,);
+        content = ImageFiltered(
+          imageFilter: ImageFilter.blur(sigmaX: 1, sigmaY: 1),
+          child: content,
+        );
       }
 
       LottieBuilder? animation;
@@ -190,42 +206,39 @@ mixin OverlayLottie<T extends StatefulWidget> on TickerProviderStateMixin<T> {
       // Create the animation, depending on the URL scheme (supported https or a string that represents the asset)
       if (animationUrl.startsWith("https://")) {
         animation = Lottie.network(
-              animationUrl,
-              repeat: false,
-              controller: _controller,
-              onLoaded: (composition) {
-                _controller
-                  ..duration = durationPerRepetition?? composition.duration
-                  ..reset()
-                  ..forward()
-                ;
-              },
-            );
+          animationUrl,
+          repeat: false,
+          controller: _controller,
+          onLoaded: (composition) {
+            _controller
+              ..duration = durationPerRepetition ?? composition.duration
+              ..reset()
+              ..forward();
+          },
+        );
       } else {
         animation = Lottie.asset(
-              animationUrl,
-              repeat: false,
-              controller: _controller,
-              onLoaded: (composition) {
-                _controller
-                  ..duration = durationPerRepetition?? composition.duration
-                  ..reset()
-                  ..forward()
-                ;
-              },
-            );
+          animationUrl,
+          repeat: false,
+          controller: _controller,
+          onLoaded: (composition) {
+            _controller
+              ..duration = durationPerRepetition ?? composition.duration
+              ..reset()
+              ..forward();
+          },
+        );
       }
 
       // Create the stack with the overlay
-      content = 
-        Stack(
-          children: [
-            IgnorePointer(child: content),
-            Center(child: animation),
-          ],
-        );
+      content = Stack(
+        children: [
+          IgnorePointer(child: content),
+          Center(child: animation),
+        ],
+      );
     }
 
     return content;
-  }    
+  }
 }
